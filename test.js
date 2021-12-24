c01283b4a890c11c3092542e994043c5e33bd574
snowtea89 <snowtea89@etoos.com>
2021-12-24 09:47:50 +0900

    test

--- test.js
+++ test.js
@@ -15,7 +15,7 @@ test('로그인', async t => {
     .click('#mArticle > div.box_floating_video > div > div > div.wrap_btn > a.link.floating_close > img')
 });
 test('로그인', async t => {
-    // Test codeㄴ
+    // Test code
     await t
     .click('#mAcle > div.box_floating_video > div > div > div.wrap_btn > a.link.floating_close > img')
 });
